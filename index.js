56a8b0c89f24e40e5a2f28faa68a32b724707abd
technolog <technologies.bro@gmail.com>
2019-09-17 15:16:16 +0300

    tests

--- index.js
+++ index.js
@@ -1,15 +1,15 @@
 const Random = require('crypto-random')
 
 const shuffle = function(deck, random = Random.value()) {
-    let length      = deck.length;
-    let clone       = deck.slice(0);
-    let shuffled    = [];
+    let length      = deck.length
+    let clone       = deck.slice(0)
+    let shuffled    = []
 
     while(length--) {
-        shuffled.push(clone.splice(Math.floor(random * length), 1).shift());
+        shuffled.push(clone.splice(Math.floor(random * length), 1).shift())
     }
 
-    return shuffled;
+    return shuffled
 };
 
-module.exports = shuffle;
+module.exports = shuffle
---
+++ test/index.js
@@ -0,0 +1,38 @@
+const assert = require('chai').assert
+const shuffle = require('../index')
+
+describe('shuffle()', function () {
+    it('should return a sorted array', function () {
+        const deck = Array.apply(null, {length: 52}).map(function (value, index) {
+            return index
+        })
+
+        const shuffledDeck = shuffle(deck);
+
+        assert.notDeepEqual(deck, shuffledDeck);
+        assert.equal(deck.length, shuffledDeck.length);
+    })
+
+    it('should accept a randomizer function', function () {
+        const deck = Array.apply(null, {length: 52}).map(function (value, index) {
+            return index
+        });
+
+        const random = function () {
+            return 0.323
+        }
+
+        const shuffledDeck = shuffle(deck, random())
+
+        assert.notDeepEqual(deck, shuffledDeck)
+        assert.equal(deck.length, shuffledDeck.length)
+    });
+
+    it('should not lose or duplicate elements', function () {
+        const deck = ['A', 'B', 'C', 'D', 'E', 'F', 'G']
+        const shuffledDeck = shuffle(deck)
+
+        assert.includeMembers(deck, shuffledDeck, 'all of original elements should be included in output')
+        assert.equal(deck.length, shuffledDeck.length, 'length of array should not change')
+    });
+});
