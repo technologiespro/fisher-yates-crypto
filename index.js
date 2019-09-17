89dad7287cc217db863724c86bee71f947d54e07
technolog <technologies.bro@gmail.com>
2019-09-17 15:08:46 +0300

    crypto random sort

---
+++ index.js
@@ -0,0 +1,15 @@
+const Random = require('crypto-random')
+
+const shuffle = function(deck, random = Random.value()) {
+    let length      = deck.length;
+    let clone       = deck.slice(0);
+    let shuffled    = [];
+
+    while(length--) {
+        shuffled.push(clone.splice(Math.floor(random * length), 1).shift());
+    }
+
+    return shuffled;
+};
+
+module.exports = shuffle;
